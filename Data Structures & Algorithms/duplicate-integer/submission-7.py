b5fe237741978584998d451b336bad7d8daf50e0
Sowreddy Pranavi <142502222+PranviSowreddy@users.noreply.github.com>
2026-05-19 10:31:33 +0530

    Add: duplicate-integer - submission-7

---
+++ Data Structures & Algorithms/duplicate-integer/submission-7.py
@@ -0,0 +1,11 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        mp={}
+        for i in nums:
+            if i in mp:
+                return True
+            mp[i]=1
+            # s.add(i)
+
+        return False    
+        
