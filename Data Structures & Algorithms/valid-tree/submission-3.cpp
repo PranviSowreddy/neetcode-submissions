ce477d4a58a2fc7ad4b1a89e161404b1884bb558
Sowreddy Pranavi <142502222+PranviSowreddy@users.noreply.github.com>
2026-06-18 12:29:47 +0530

    Add: valid-tree - submission-3

---
+++ Data Structures & Algorithms/valid-tree/submission-3.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+
+int find(int x,vector<int>&parent)
+{
+    if(x==parent[x])return x;
+    return parent[x]=find(parent[x],parent);
+}
+    bool validTree(int n, vector<vector<int>>& edges) {
+        vector<int>parent(n);
+        for(int i=0;i<n;i++)
+        parent[i]=i;
+
+        vector<int>size(n,1);
+
+        int components=n;
+
+        for(auto& edge:edges)
+        {
+            int u=edge[0];
+            int v=edge[1];
+
+            int pa=find(u,parent);
+            int pb= find(v,parent);
+
+            if(pa!=pb)
+            {
+                components--;
+                if(size[pa]<size[pb])
+                {
+                    size[pb]+=size[pa];
+                    parent[pa]=pb;
+                }
+                else
+                {
+                    size[pa]+=size[pb];
+                    parent[pb]=pa;
+                }
+            }
+            //else return false;
+        }
+     
+       
+        return (components==1 && edges.size()==n-1);
+        
+    }
+};
